affdfb08b550115997a4a9151f602b109a2765c3
bheart7 <bheartshrestha7@gmail.com>
2021-08-12 14:54:26 +0545

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
