9c706c4930650d1d6879a65a47675557b04afd03
bheart7 <bheartshrestha7@gmail.com>
2021-08-12 14:58:35 +0545

    Card added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div class="card" style={{ width: "18 rem" }}>
+      <img class="card-img-top" src="" alt="Card image cap" />
+      <div class="card-body">
+        <h5 class="card-title">Title</h5>
+        <p class="card-text">Description will be added</p>
+        <a href="#" class="btn btn-primary">
+          Add to Cart
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
